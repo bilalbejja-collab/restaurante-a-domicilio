064a818a624bee51befdad2394550c0250b36ff0
Bilal <you@example.com>
2021-02-20 23:00:24 +0100

    first commit

--- resources/views/auth/login.blade.php
+++
@@ -1,2 +0,0 @@
-<!--PARA USAR LA PLANTILLA DE ADMINLTE-->
-@extends('adminlte::auth.login')
--- resources/views/categoria/show.blade.php
+++
@@ -1,6 +0,0 @@
-<tbody divide-y divide-gray-200>
-    <tr class="text-small text-gray-500">
-        <td class="px-6 py-4">{{ $categoria->id }}</td>
-        <td class="px-6 py-4">{{ $categoria->nombre }}</td>
-    </tr>
-</tbody>
