95bc697e1b8b3c6716393c5ef5dff05abf92a6a0
Bilal <you@example.com>
2021-03-01 16:31:25 +0100

    Solucionar problemas de subir imagen a heroku

--- config/filesystems.php
+++ config/filesystems.php
@@ -34,11 +34,18 @@ return [
             'driver' => 'local',
             'root' => storage_path('app'),
         ],
-
+        /*
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'storage',
+            'visibility' => 'public',
+        ],
+*/
+        'public' => [
+            'driver' => 'local',
+            'root' => public_path(),
+            'url' => env('APP_URL') . '/public',
             'visibility' => 'public',
         ],
 
