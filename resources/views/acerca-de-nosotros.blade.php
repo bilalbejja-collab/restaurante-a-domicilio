e31d84b1a0dabd066cec6c7a193ca2397f0dd5d5
Bilal <you@example.com>
2021-03-08 00:44:18 +0100

    Haciendo la parte acerca de nosotros

--- resources/views/acerca-de-nosotros.blade.php
+++ resources/views/acerca-de-nosotros.blade.php
@@ -1,5 +1,9 @@
 <x-app-layout>
-    <div class="container py-8">
-        Acerca De Nosotros
+    <div class="container py-8 text-center text-gray-600 mb-5 font-mono text-xl">
+        <div class="mt-2">
+            <p class="mt-2"><strong>Teléfono:</strong> +34 12 34 56 789 / 987</p>
+            <p class="mt-2"><strong>Correo electrónico:</strong> restaurante-a-domicilio@cliente.es</p>
+            <p class="mt-2">Este es un ejemplo muy simple de cómo si trabaja un restaurante a domicilio. Hecho por el alumno Bilal Bejja, estudiante 2daw. Espero que os guste.</p>
+        </div>
     </div>
 </x-app-layout>
