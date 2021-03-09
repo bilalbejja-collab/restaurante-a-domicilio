f1a0030ed7dea1a65fb400ab10d8256859e298a4
Bilal <you@example.com>
2021-03-09 17:15:04 +0100

    Corregiendo un error en heroku cuando un plato no tiene foto

--- resources/views/carrito/checkout.blade.php
+++ resources/views/carrito/checkout.blade.php
@@ -63,33 +63,31 @@
                                                     {{ $plato->quantity }}
                                                 </td>
                                                 <!-- Si querriamos modificar la cantidad seria buena idea usar los metodos del carrito add y remove
-                                                                    <td class="px-6 py-4 whitespace-nowrap">
-                                                                        <div class="inline-block flex">
+                                                        <td class="px-6 py-4 whitespace-nowrap">
+                                                            <div class="inline-block flex">
 
-                                                                            {{ $plato->quantity }}
+                                                                {{ $plato->quantity }}
 
-                                                                            <form action="{{ route('carro.add') }}" method="POST">
-                                                                                @csrf
-                                                                                <input type="hidden" name="plato_id"
-                                                                                    value="{{ $plato->id }}">
-                                                                                <button type="submit">
-                                                                                    +
-                                                                                </button>
-                                                                            </form>
-                                                                        </div>
-                                                                    </td>
-                                                                    -->
+                                                                <form action="{{ route('carro.add') }}" method="POST">
+                                                                    @csrf
+                                                                    <input type="hidden" name="plato_id"
+                                                                        value="{{ $plato->id }}">
+                                                                    <button type="submit">
+                                                                        +
+                                                                    </button>
+                                                                </form>
+                                                            </div>
+                                                        </td>
+                                                        -->
                                                 <td class="px-6 py-4 whitespace-nowrap">
-                                                    @if ($plato->image['urlfoto'])
-                                                        {{ $plato->image['urlfoto'] }}
-                                                    @endif
+
                                                 </td>
                                                 <td class="px-6 py-4 whitespace-nowrap">
                                                     <form action="{{ route('carro.borrar') }}" method="post">
                                                         @csrf
                                                         <input type="hidden" name="id" value="{{ $plato->id }}">
                                                         <button type="submit" class="btn btn-link btn-sm text-danger">
-                                                            <i class="fas fa-trash"></i>
+                                                            <i class="fas fa-trash">X</i>
                                                         </button>
                                                     </form>
                                                 </td>
