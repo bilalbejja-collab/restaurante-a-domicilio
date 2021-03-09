6cd25bc852bccf6c6f9bf0c0d17eb88e3b55dc91
Bilal <you@example.com>
2021-03-09 19:36:13 +0100

    Corregiendo un error en heroku cuando un plato no tiene foto

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -23,7 +23,7 @@ class ImageFactory extends Factory
     {
         return [
             //'url' => 'platos/' . $this->faker->image(public_path('storage/platos'), 640, 400, null, false) //false: para que solo me coge el nombre de la imagen
-            'url' => $this->faker->image('public/storage/platos', 640, 400, null, false) //false: para que solo me coge el nombre de la imagen
+            'url' => 'platos/' . $this->faker->image('public/storage/platos', 640, 400, null, false) //false: para que solo me coge el nombre de la imagen
         ];
     }
 }
